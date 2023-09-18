4fd9945a87d1bf7a0bb45a6d1003478c68ede852
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-09-18 21:17:22 +0200

    start

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Post } from 'src/app/model/post';
+import { StorageService } from 'src/app/services/storage.service';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+
+  postsArray:Post[]=[]
+  constructor(public storage:StorageService){
+    
+
+  }
+
+}
